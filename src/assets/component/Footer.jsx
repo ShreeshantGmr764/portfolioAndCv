ac91723dbe5ffd10a3c0e26dcb2a8347d7209d36
ShreeshantGmr764 <withgamergaming764@gmail.com>
2024-10-24 11:06:45 +0545

    Github&Linkedin

--- src/assets/component/Footer.jsx
+++ src/assets/component/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     <footer class="bg-white py-6 text-center">
         <p class="text-gray-600">&copy; 2024 Shreeshant Ghimire. All rights reserved.</p>
         <div class="mt-4">
-            <a href="https://linkedin.com" class="text-indigo-600 hover:underline mx-2">LinkedIn</a>
-            <a href="https://github.com" class="text-indigo-600 hover:underline mx-2">GitHub</a>
+            <a href="https://www.linkedin.com/in/shreeshant-gmr-501468288/" class="text-indigo-600 hover:underline mx-2">LinkedIn</a>
+            <a href="https://github.com/ShreeshantGmr764" class="text-indigo-600 hover:underline mx-2">GitHub</a>
         </div>
     </footer>
     </>
